4f6e7f3b90a2623f26bf7b2f28b0eb4ac19e4ca6
liqiang <565384027@qq.com>
2018-05-24 20:48:23 +0800

    code of chapter 1-3

---
+++ src/Chapter1_1Low/DecimalToBinary.java
@@ -0,0 +1,16 @@
+package Chapter1_1Low;
+
+import edu.princeton.cs.algs4.StdOut;
+
+public class DecimalToBinary {
+    public static String decimalToBinary(int n){
+        String resultString="";
+        for(int i=31;i>=0;i--)
+            resultString=resultString+(n>>>i&1);  //将整数每一个高位右移至相应的最低位再“与”1，结果转换为字符串从左到右排列输出，依然是原来的二进制
+        return resultString;
+    }
+    public static void main(String[] args){
+
+        StdOut.println(decimalToBinary(97));
+    }
+}
